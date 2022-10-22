82243b8b2de551f8fc2440d85374921d4baafc3a
Lucie Clemot <lucie.clemot@student-cs.fr>
2022-10-22 14:22:04 +0200

    PCA commentée

--- PCA.py
+++ PCA.py
@@ -1,63 +1,52 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
-from sklearn import model_selection
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
-from sklearn.cross_decomposition import PLSRegression, PLSSVD
-from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
+
 from preprocessing_wrapper import load_preprocessed_data
 import numpy as np
 
 data = load_preprocessed_data()
+data = data.drop(columns = ["Listing_ID", "Host_ID", "Postal_Code"])
 features = data.columns.drop('Price')
 target = 'Price'
 
+
+
+"""
+Ce script est composé de deux fonctions. La première, airbnb_PCA, est faite pour déterminer le nombre optimal
+de PCA components. Pour que le code finisse de tourner, il faut fermer le graphe qui s'ouvrira sur votre IDE.
+A partir du graphe, choisir la valeur n (dès que la variance cumulative expliquée dépasse la barre des 95%.
+Appliquer la seconde fonction aribnb_PCA_n pour obtenir les coordonnées PCA."""
+
+
 def airbnb_PCA(df, features, target):
+    df.dropna(inplace=True)
     # Separating out the features
     x = df.loc[:, features].values
     # Separating out the target
     y = df.loc[:,target].values
     # Standardizing the features
     x = StandardScaler().fit_transform(x)
+    # Create the PCA regressor with a maximum number of components
     pca = PCA()
-    principalComponents = pca.fit_transform(x)
-    principalDf = pd.DataFrame(data = principalComponents
-    #                           ,columns = ['PC'+str(i) for i in range(1,n+1)]
-                            )
-
+    PrincipalComponents = pca.fit_transform(x)
+    principalDf = pd.DataFrame(data = PrincipalComponents)
+    # Ensure that the PCA values will merge to the target values
     principalDf.index = df.index
-
-    print(principalDf.shape)
-    print(df2[target].shape)
-    finalDf = pd.concat([principalDf, pd.Series(df2[target])], axis = 1)
-    print(finalDf.shape)
-    explained_variance = pca.explained_variance_ratio_
+    # Concatenante the PCA components values to the target balues
+    finalDf = pd.concat([principalDf, pd.Series(df[target])], axis = 1)
+    # Create the explained variance
+    pca2 = pca.fit(x)
+    explained_variance = pca2.explained_variance_ratio_
     print(explained_variance)
     # Plot the cumulative variance to identify optimal n_components
-    fig, ax = plt.subplots()
-    xi = np.arange(1, 4, step=1)
-    y = np.cumsum(pca.explained_variance_ratio_)
-    plt.ylim(0.0,1.1)
-    plt.plot(xi, y, marker='o', linestyle='--', color='b')
-    plt.xlabel('Number of Components')
-    plt.xticks(np.arange(0, 20, step=50)) #change from 0-based array index to 1-based human-readable label
-    plt.ylabel('Cumulative variance (%)')
-    plt.title('The number of components needed to explain variance')
-
-    plt.axhline(y=0.95, color='r', linestyle='-')
-    plt.text(0.5, 0.85, '95% cut-off threshold', color = 'red', fontsize=16)
-
-    ax.grid(axis='x')
+    plt.plot(np.cumsum(pca2.explained_variance_ratio_), linewidth=2)
+    plt.xlabel('Components')
+    plt.ylabel('Cumulative sum of explained Variances')
     plt.show()
 
-    # Return PCA fields and target variable.
-    return(finalDf)
-
-#test = airbnb_PCA(data, features, target)
-#print(test)
+airbnb_PCA(data, features, target)
 
 def airbnb_PCA_n(df, features, target,n):
     # Separating out the features
@@ -66,21 +55,23 @@ def airbnb_PCA_n(df, features, target,n):
     y = df.loc[:,target].values
     # Standardizing the features
     x = StandardScaler().fit_transform(x)
+    # Create the PCA regressor with the optimal number of components n
     pca = PCA(n)
     principalComponents = pca.fit_transform(x)
+    # Create the new dataframe
     principalDf = pd.DataFrame(data = principalComponents
                                ,columns = ['PC'+str(i) for i in range(1,n+1)]
                             )
-
+    # Ensure that the PCA values will merge to the target values      
     principalDf.index = df.index
-
-    print(principalDf.shape)
-    print(df2[target].shape)
-    finalDf = pd.concat([principalDf, pd.Series(df2[target])], axis = 1)
+    # Concatenante the PCA components values to the target balues
+    finalDf = pd.concat([principalDf, pd.Series(df[target])], axis = 1)
     print(finalDf.shape)
+    # Create the explained variance
     explained_variance = pca.explained_variance_ratio_
     print(explained_variance)
     # Return PCA fields and target variable.
     return(finalDf)
 
-print(data.loc[5])
+test = airbnb_PCA_n(data, features, target, 80)
+print(test)
