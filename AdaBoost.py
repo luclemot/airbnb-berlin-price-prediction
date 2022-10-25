788ed7679b67e1feed42f73960cf3eb365be10bc
Saad Chtouki <saad.chtouki@student-cs.fr>
2022-10-25 16:15:56 +0200

    Cross validation + Modification AdaBoost

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
 from sklearn import metrics
 import numpy as np
 from PCA import airbnb_PCA_n
-from preprocessing_wrapper import load_preprocessed_data
+from preprocessing_wrapper import (load_preprocessed_data, cross_validation)
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.model_selection import GridSearchCV
 
@@ -18,14 +17,12 @@ A partir des paramètres retournés par la fct searchAdaBoost_parameters, on peu
 def searchAdaBoost_parameters(pca:bool=True): #the execution can take 20 minutes // Result : Best_params:{n_estimators:50, learning_rate:0.0005}
     data = load_preprocessed_data()
     data = data.drop(columns = ["Listing_ID", "Host_ID", "Postal_Code"])
-    if pca:
-        data = airbnb_PCA_n(data, features, target, 80)
     X = data.drop(columns=['Price'])
     Y = data[['Price']]
-    features = data.columns.drop("Price")
-    target = 'Price'
     # Create the training and testing set
     x_train, x_test,y_train,y_test = train_test_split(X,Y,test_size =0.3)
+    if pca:
+        x_train, x_test = airbnb_PCA_n(x_train, x_test, 80)
     #Create AdaBoost Regressor
     ada=AdaBoostRegressor()
     #Parameters to test for a GridSearch
@@ -38,29 +35,15 @@ def searchAdaBoost_parameters(pca:bool=True): #the execution can take 20 minutes
     pred = grid_search.best_estimator_.predict(x_test)
     rmse = metrics.mean_squared_error(y_test, pred, squared = False)
     print('rmse :',rmse)
-
+    
 def adaBoost(nb_estimators,learn_rate, pca:bool=True):
     data = load_preprocessed_data()
     data = data.drop(columns = ["Listing_ID", "Host_ID", "Postal_Code"])
-    if pca:
-        data = airbnb_PCA_n(data, features, target, 80)
     X = data.drop(columns=['Price'])
     Y = data[['Price']]
-    features = data.columns.drop("Price")
-    target = 'Price'
-    # Create the training and testing set
-    x_train, x_test,y_train,y_test = train_test_split(X,Y,test_size =0.3)
     #Create AdaBoost Regressor
     ada=AdaBoostRegressor(n_estimators=nb_estimators, learning_rate=learn_rate)
-    # Fit on the training set
-    ada.fit(x_train, y_train)
-    # Create predictions
-    pred = ada.predict(x_test)
-    # Print the accuracy and root mean square error
-    acc = ada.score(x_test,y_test)
-    print(acc)
-    rmse = metrics.mean_squared_error(y_test, pred, squared = False)
-    print(rmse)
-    return(rmse)
+    #Apply Cross validation
+    return cross_validation(ada,5,X,Y,pca)
 
 #adaBoost(50,0.0005,True)
