6492592538ec435785176894d83e767562136882
Robin Delebassée <robin.delebassee@student-cs.fr>
2022-10-28 18:31:31 +0200

    Update best models file with xgboost regressor

--- Best_Models.py
+++ Best_Models.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
-from Random_Forest import (
-    Random_Forest
-)
+
+from Random_Forest import Random_Forest
+from xgBoost import xgboost
 
 
 def get_data_split():
@@ -24,12 +24,32 @@ def get_data_split():
 
 X_train, X_test, y_train, y_test = get_data_split() 
 
-regressor = Random_Forest(X_train, X_test, y_train, y_test, max_depth = 49 , max_features = 'sqrt', n_estimators = 900)
+regressor_rf = Random_Forest(X_train, X_test, y_train, y_test, max_depth = 49 , max_features = 'sqrt', n_estimators = 900)
+y_pred_rf = regressor_rf.predict(X_test)
+RMSE_rf = np.sqrt(mean_squared_error(y_test, y_pred_rf))
+R2_rf = r2_score(y_test,y_pred_rf)
 
-y_pred = regressor.predict(X_test)
-RMSE = np.sqrt(mean_squared_error(y_test, y_pred))
-R2 = r2_score(y_test,y_pred)
+        
+regressor_xgb = xgboost(
+    objective="reg:squarederror",
+    random_state=42,
+    max_depth=5,
+    subsample=1,
+    colsample_bytree=0.4,
+    learning_rate=0.05,
+    gamma=0,
+    eta=0.1,
+    n_estimators=100
+)
+y_pred_xgb = regressor_xgb.predict(X_test)
+RMSE_xgb = np.sqrt(mean_squared_error(y_test, y_pred_xgb))
+R2_xgb = r2_score(y_test,y_pred_xgb)
+
 
 print('Pour le Random Forest on obtient')
-print('RMSE :', RMSE)
-print('RMSE :', R2)
+print('RMSE :', RMSE_rf)
+print('RMSE :', R2_rf)
+
+print('Pour le XGBoost on obtient')
+print('RMSE :', RMSE_xgb)
+print('RMSE :', R2_xgb)
